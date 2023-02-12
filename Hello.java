98d494592871037a444680b806af46d3bbb7a873
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2023-02-11 23:05:36 -0300

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,26 @@
+
+package hello;
+  //1-Crie  um  programa com a frase do Hello word em três linhas como o exemplo a baixo.
+// ------------------------------------------------------------------------------------------------------------
+//                                                 Hello Word
+//                                                 Hello Word
+//                                                 Hello Word
+//---------------------------------------------------------------------------------------------------------------
+
+public class Hello {//PrimeiroPrograma  e  //Bloco de Entrada
+	public static void main(String[] args) { //Bloco de Entrada
+	
+  	System.out.println("----------------------------");	
+	System.out.println("       Hello Word"+"\n"); 	
+	System.out.println("       Hello Word"+"\n");
+	System.out.println("       Hello Word"+"\n");
+        System.out.println("----------------------------");	
+	
+	}	//Bloco de Saida.
+	
+} // Bloco de Saida.
+
+
+    
+    
+
