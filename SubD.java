6f4508f9c21883a54bf3daf1c3b304f4d5e0074a
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2023-02-17 15:14:19 -0300

    Add files via upload

---
+++ SubD.java
@@ -0,0 +1,55 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */   
+
+package subd;
+
+import java.util.Scanner;
+
+public class SubD{  //Nome da Classe e entrada do bloco.
+	public static void main(String[] args) {
+	
+	Scanner ler= new Scanner(System.in);//Comando utilizado para fazer a leitura dos dados digitados na leitura.
+	
+       //Declaração das Variaveis
+	int Sub=0;//Zero para não ocorrer erro.
+	int Divi=0;//Zero para não ocorrer erro.
+	int numeroD;
+	
+	System.out.println("Informe o numero para divisao"+"\n");// Entrada de dado ou Entrada de leitura.
+	numeroD=ler.nextInt();                                   //Saida de dado ou Saida de leitura.
+	
+	Sub=53-3;//Conta.
+	Divi=Sub/numeroD;//Conta.
+	
+        //Imprimir os Resultados Finais.
+	System.out.println("       A Matematica Realizada  de Subtracao      ");
+	System.out.println("********************************************"+"\n");
+	System.out.println("O resultado da Subtracao"+"\n"+Sub);
+	System.out.println("*********************************************"+"\n"+"\n");
+	
+	System.out.println("       A Matematica Realizada de Divisao           ");
+	System.out.println("**********************************************"+"\n");
+	System.out.println("O resultado da Divisao"+"\n"+Divi);
+	System.out.println("**********************************************"+"\n"+"\n");
+	
+	
+	
+	
+	}//Saída do bloco.
+
+}//Saída do bloco.
+
+
+
+
+
+
+
+
+        
+       
+    
+    
+
