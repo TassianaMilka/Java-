763fbbb9779a545e84679bbfb51ef5bb02e6faa9
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2023-03-28 16:17:22 -0300

    6

---
+++ Codigo.java
@@ -0,0 +1,64 @@
+package codigo;
+
+//Programando em Java(6) or Programming in Java(6).
+//Em uma loja necessitam realizar e saber se o código 39060 e o 18050 esta armazenado.
+
+import java.util.Scanner;
+
+public class Codigo {// Nome da Classe e entrada de bloco.
+
+/**
+ * @param args
+ */
+public static void main(String[] args) {//Entrada de dado.
+  //Entrada de bloco
+  
+    
+ Scanner ler = new Scanner(System.in);//Comando utilizado para fazer a leitura dos dados digitados na leitura.
+
+    
+    
+   //Declaração das variáveis.
+ 
+    int numeros1;
+    int numeros2;
+
+    
+    System.out.println("Informa o código\n");//Entrada de dado.
+    numeros1=ler.nextInt();//Saída de dado.
+     
+      //Validação do que foi informado se o código1 está armazenado.
+      if(numeros1==39060){
+
+       System.out.println("O código armazenado\n");
+
+
+      }else{
+
+          System.out.println("O código não armazenado\n");
+
+      }
+
+
+
+    System.out.println("Infome o código \n");//Entrada de dado.
+    numeros2=ler.nextInt();//Saída de dado.
+
+
+
+    //Validação do que foi informado se o código2 está armazenado.
+    if(numeros2==18050){
+
+
+      System.out.println("O código armazenado");
+    }else{
+
+
+      System.out.println("O código não armazenado");
+    }
+
+
+
+  }//Saída de bloco.
+
+}//Saída de dado.
